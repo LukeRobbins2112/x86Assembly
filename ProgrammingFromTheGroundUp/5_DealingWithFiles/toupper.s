ec9f2f225e99be616973e483eb481fd72d590550
Luke <lukerobbins2112@gmail.com>
2019-12-11 11:10:07 -0500

    Replaced hardcoded system call values with constants e.g. SYS_OPEN

--- ProgrammingFromTheGroundUp/5_DealingWithFiles/toupper.s
+++ ProgrammingFromTheGroundUp/5_DealingWithFiles/toupper.s
@@ -1,20 +1,35 @@
+#####################################
 	.section .data
+#####################################
 
 input_file:
 	.asciz "input.txt"
 output_file:
 	.asciz "output.txt"
 
+# file constants
 	.equ O_CREAT_WRONLY_TRUNC, 03101
 
 	.equ INPUT_FD, 	-8
-	.equ OUTPUT_FD, -16 
+	.equ OUTPUT_FD, -16
 
+# syscall constants
+	.equ SYS_READ, 	0
+	.equ SYS_WRITE,	1
+	.equ SYS_OPEN,	2
+	.equ SYS_CLOSE,	3
+	.equ SYS_EXIT,	60
+
+#####################################	
 	.section .bss
+#####################################
+	
+	.lcomm DATA_BUFFER, 512
 
-	.lcomm INPUT_BUFFER, 512
-
+#####################################	
 	.section .text
+#####################################
+
 	.global _start
 
 _start:
@@ -23,7 +38,7 @@ _start:
 
 open_input:	
 	#open file syscall
-	movq $2,%rax
+	movq $SYS_OPEN,%rax
 	# movq $input_file,%rdi
 	movq 16(%rbp), %rdi
 	movq $0,%rsi
@@ -34,7 +49,7 @@ open_input:
 	movq %rax, INPUT_FD(%rbp)
 
 open_output:
-	mov $2,%rax
+	mov $SYS_OPEN,%rax
 	mov $output_file,%rdi
 	mov $O_CREAT_WRONLY_TRUNC,%rsi
 	mov $0666,%rdx
@@ -45,9 +60,9 @@ open_output:
 
 	
 loop:
-	movq $0, %rax	# read
+	movq $SYS_READ, %rax	
 	movq INPUT_FD(%rbp), %rdi
-	movq $INPUT_BUFFER, %rsi
+	movq $DATA_BUFFER, %rsi
 	movq $512, %rdx
 	syscall
 
@@ -57,23 +72,23 @@ loop:
 
 	# write data to output
 	movq %rax, %rdx			# buffer size to write
-	movq $1, %rax			# write system call
+	movq $SYS_WRITE, %rax		# write system call
 	movq OUTPUT_FD(%rbp), %rdi	# output fd
-	movq $INPUT_BUFFER, %rsi	# buffer towrite
+	movq $DATA_BUFFER, %rsi		# buffer towrite
 	syscall
 	
 	jmp loop
 	
 close_files:
-	movq $3,%rax			# close system call
+	movq $SYS_CLOSE, %rax			# close system call
 	movq INPUT_FD(%rbp), %rdi		# get file descriptor - @TODO use common register / address?
 	syscall
 
-	movq $3, %rax
+	movq $SYS_CLOSE, %rax
 	movq OUTPUT_FD(%rbp), %rdi
 	syscall
 
 exit:
-	mov $60,%rax	# EXIT
+	mov $SYS_EXIT, %rax	# EXIT
 	mov $0,%rdi	# 0 return code = success
 	syscall
