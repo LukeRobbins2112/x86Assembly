56c9a3c217f31d512f344159a8cab21054841cdb
Luke <lukerobbins2112@gmail.com>
2019-12-13 10:50:29 -0500

    Implemented program to loop through individual records in a .dat file and print the first names

---
+++ ProgrammingFromTheGroundUp/6_ReadingWritingSampleRecords/write-record.s
@@ -0,0 +1,31 @@
+
+
+	.include "linux.s"
+	.include "record-def.s"
+
+
+	.section .text
+
+	.equ WRITE_BUF, 16
+	.equ OUTPUT_FD, 24
+
+	.globl write_record
+	.type write_record @function
+
+write_record:
+
+	# save stack pointer
+	pushq %rbp
+	movq %rsp, %rbp
+
+	# write record from buffer
+	movq $SYS_WRITE, %rax
+	movq OUTPUT_FD(%rbp), %rdi
+	movq WRITE_BUF(%rbp), %rsi
+	movq $RECORD_SIZE, %rdx
+	syscall
+
+	# done writing - return number of bytes read as result
+	movq %rbp, %rsp
+	popq %rbp
+	retq
