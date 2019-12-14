738e6c25460408cabf5660291a023c16f7d0b128
Luke <lukerobbins2112@gmail.com>
2019-12-14 16:48:08 -0500

    Moved original printf and hello world shared library examples to their own subdirectories

--- ProgrammingFromTheGroundUp/8_SharingFunctionsWithCodeLibraries/build_helloworld_lib.sh
+++
@@ -1,6 +0,0 @@
-
-# build the program object file
-as helloworld-lib.s -o helloworld-lib.o
-
-# dynamically link with ld-linux and libc
-ld -o helloworld-lib -dynamic-linker /lib64/ld-linux-x86-64.so.2 helloworld-lib.o -lc
